38195c816cc279d0f7ae50605183732e234543ef
Noxsense <noxsense@gmail.com>
2024-01-21 17:46:11 +0100

    Refactor main (First Step, Part 2)
    main should load a configuration and the save.
    
    That configuration and save is then used to setup the wudel world,
    render and simulation.
    
    Add some better return types

--- src/main.rs
+++ src/main.rs
@@ -7,31 +7,39 @@
 //! ## Author
 //! Ngoc (Nox) Le <noxsense@gmail.com>
 
+type Config = usize;
+type Save = u64;
+
 /// The main method of the wusel world.
 fn main() -> Result<(), std::io::Error> {
     env_logger::init();
-    let config = load_configuration().unwrap();
-    let save = load_save();
+    let config = load_configuration("res/config.yaml").unwrap();
+    let save = load_save().unwrap_or_else(new_save);
     run(config, save)
 }
 
-fn load_configuration() -> Result<usize, std::io::Error> {
+
+fn load_configuration(config_file_name: &str) -> Result<Config, std::io::Error> {
+    let file = std::fs::File::open(config_file_name);
+    if let Err(error) = file {
+        return Err(error);
+    }
     Ok(42usize)
 }
 
-fn load_save() -> u64 {
-    42u64
+fn load_save() -> Option<Save> {
+    Some(42u64)
 }
 
-fn run(
-    config: usize,
-    save: u64
-    ) -> Result<(), std::io::Error> {
+fn new_save() -> Save {
+    21u64
+}
+
+fn run(config: Config, save: Save) -> Result<(), std::io::Error> {
     println!("Configuration: {}", config);
     println!("Save:          {}", save);
     Ok(())
 }
-
 
 //////
 
@@ -40,14 +48,14 @@ mod main_test {
     #[test]
     fn should_loads_default_configloading() {
         // TODO setup: with configuration file
-        assert_eq!(42usize, crate::load_configuration().unwrap());
+        assert_eq!(42usize, crate::load_configuration("res/config.yaml").unwrap());
     }
 
     #[test]
     fn should_fails_if_no_configuration_is_found() {
         // TODO setup: no configuration file
-        if let Err(error) = crate::load_configuration() {
-            assert_eq!("Config Not Found", error.to_string());
+        if let Err(error) = crate::load_configuration("/xxx/not_existent.yaml") {
+            assert_eq!("No such file or directory (os error 2)", error.to_string());
         } else {
             assert!(false, "The Configuration was not found and should fail")
         }
@@ -56,12 +64,12 @@ mod main_test {
     #[test]
     fn should_loads_last_save_if_available() {
         // TODO setup with save file
-        assert_eq!(42u64, crate::load_save());
+        assert_eq!(Some(42u64), crate::load_save());
     }
 
     #[test]
     fn should_loads_empty_save_if_not_provided() {
         // TODO setup no save file
-        assert_eq!(42u64, crate::load_save());
+        assert_eq!(None, crate::load_save());
     }
 }
